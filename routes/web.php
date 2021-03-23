4ebcefcc006a5e7a02f5c8ea21c99f8fe9ec7ed2
池田将之 <masayuki.ikeda@connec10.co.jp>
2021-03-23 23:00:52 +0900

    Laravel６系はAuth関連使うには「laravel/ui」インストール必要あるらしい ※下記使用コマンド $ composer require laravel/ui $ php artisan ui vue --auth

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Auth
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Top
 Route::get('/', 'HomeController@index')->name('home');
